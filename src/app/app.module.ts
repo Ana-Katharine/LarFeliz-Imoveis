fc35846ff3f23e923ca44b9988b242d43e2c7ddc
Ana Karina <ana_silvaleitao@outlook.com.br>
2023-07-28 16:41:12 -0300

    Organizando Cabeçalho

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//*BLOCO DE CÓDIGO*//
 import { AppComponent } from './app.component';
 import { QuemSomosComponent } from './quem-somos/quem-somos.component';
 import { ContatoComponent } from './contato/contato.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
 
+//*MATERIAL UI*//
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     QuemSomosComponent,
-    ContatoComponent
+    ContatoComponent,
+    CabecalhoComponent
   ],
   imports: [
     BrowserModule,
